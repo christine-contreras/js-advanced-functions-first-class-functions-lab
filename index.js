90bb59b4698dff9cd71dbba5b7cd4583add71f8f
Christine Contreras <christineec23@yahoo.com>
2021-04-07 17:51:12 -0500

    adding in notes

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const returnFirstTwoDrivers = function(array) {
     };
  }
 
+ //to envoke on your own 
+ //createFareMultiplier(3)(10);
+ //createFareMultiplier(integer)(fare);
 
  //Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareDoubler() function accepts a fare as its lone argument and doubles it.
  const fareDoubler = createFareMultiplier(2);
